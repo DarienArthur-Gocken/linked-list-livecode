8a926c0e464bdb1070c7c71028b134a545b9f4ae
Darien Arthur-Gocken <233949874+DarienArthur-Gocken@users.noreply.github.com>
2026-01-15 14:52:06 -0800

    livecode 2

--- src/Practice.java
+++ src/Practice.java
@@ -6,15 +6,41 @@ public class Practice {
         ryan.next = new Node('t', null);
         ryan.next.next = new Node('x', null);
 
-        head = head.next;
+        printList(head);
+       // System.out.println(contains(head, 'x'));
+        // System.out.println(contains(head, 'e'));
+
+        System.out.println("-----");
+        remove(head, 't');
         printList(head);
     }
 
     public static void printList(Node start) {
-        Node current = start;
-        while(current != null) {
-            System.out.println(current.value);
+        while(start != null) {
+            System.out.println(start.value);
+            start = start.next;
+        }
+    }
+
+    public static boolean contains(Node start, char toFind) {
+        while(start != null) {
+            if(start.value == toFind) {
+                return true;
+            }
+            start = start.next;
+        }
+        return false;
+    }
+
+    public static char remove(Node head, char toRemove) {
+        Node current = head;
+        while(current.next != null) {
+            if(current.next.value == toRemove) {
+                current.next = current.next.next;
+                return toRemove;
+            }
             current = current.next;
         }
+        return '\0';
     }
 }
