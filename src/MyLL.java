f3da79ace6d7bb31db4d7b4d67c6a9bed9047f98
Darien Arthur-Gocken <233949874+DarienArthur-Gocken@users.noreply.github.com>
2026-01-20 14:51:17 -0800

    livecode 3

--- src/MyLL.java
+++ src/MyLL.java
@@ -1,33 +1,43 @@
-public class MyLL {
-    private class Node {
-        public char value;
-        public Node next;
+public class MyLL<T> {
+    private class Node<E> {
+        public E value;
+        public Node<E> next;
+        public Node<E> previous;
 
-        public Node(char value, Node next) {
+        public Node(E value, Node<E> next, Node<E> previous) {
             this.value = value;
             this.next = next;
+            this.previous = previous;
+            if(this.next != null) {
+              this.next.previous = this;
+            }
+            if(this.previous != null) {
+                this.previous.next = this;
+            }
         }
     }
 
-    private Node head;
+    private Node<T> head;
+    private Node<T> tail;
 
     public MyLL() {
         head = null;
+        tail = null;
     }
 
 
     public void printList() {
-        Node current = head;
+        Node<T> current = head;
         while(current != null) {
             System.out.println(current.value);
             current = current.next;
         }
     }
 
-    public boolean contains(char toFind) {
-        Node current = head;
+    public boolean contains(T toFind) {
+        Node<T> current = head;
         while(current != null) {
-            if(current.value == toFind) {
+            if(current.value.equals(toFind)) {
                 return true;
             }
             current = current.next;
@@ -35,21 +45,53 @@ public class MyLL {
         return false;
     }
 
-    public char remove(char toRemove) {
-        if(head == null) return '\0';
-        if(head.value == toRemove) {
+    public void addToBack(T toAdd) {
+        if(tail != null) {
+            Node<T> newNode = new Node<>(toAdd, null, tail);
+            tail = newNode;
+            if(tail.previous.previous == null) {
+                head = tail.previous;
+            }
+        } else {
+            Node<T> newNode = new Node<>(toAdd, null, null);
+            head = newNode;
+            tail = newNode;
+        }
+        
+    }
+
+    public void printFromBack() {
+        if(tail == null) return;
+        Node<T> current = tail;
+        while(current != null) {
+            System.out.println(current.value);
+            current = current.previous;
+        }
+    }
+
+    public T remove(T toRemove) {
+        if(head == null) return null;
+        if(head.value.equals(toRemove)) {
             head = head.next;
+            head.previous = null;
+            return toRemove;
+        }
+        if(tail.value.equals(toRemove)) {
+            tail = tail.previous;
+            tail.next = null;
             return toRemove;
         }
 
-        Node current = head;
+        Node<T> current = head;
         while(current.next != null) {
-            if(current.next.value == toRemove) {
+            if(current.next.value.equals(toRemove)) {
                 current.next = current.next.next;
+                current.next.previous = current;
                 return toRemove;
             }
             current = current.next;
+            current.next.previous = current;
         }
-        return '\0';
+        return null;
     }
 }
--- src/Practice.java
+++ src/Practice.java
@@ -1,10 +1,13 @@
 public class Practice {
     public static void main(String[] args) {
+        /*
         Node head = new Node('g', null);
         Node ryan = new Node('s', null);
         head.next = ryan;
         ryan.next = new Node('t', null);
         ryan.next.next = new Node('x', null);
+
+
 
         printList(head);
        // System.out.println(contains(head, 'x'));
@@ -12,7 +15,45 @@ public class Practice {
 
         System.out.println("-----");
         remove(head, 't');
-        printList(head);
+        printList(head);*/
+/*
+        MyLL<Character> list = new MyLL<>();
+        list.addToBack('x');
+        list.addToBack('m');
+        list.addToBack('v');
+        list.addToBack('q');
+
+        list.printList();
+
+        MyLL<Integer> list2 = new MyLL<>();
+        list2.addToBack(5);
+        list2.addToBack(99);
+        list2.addToBack(2);
+        list2.addToBack(-33333);
+        list2.addToBack(66);
+
+        list2.printList();*/
+/*
+        MyLL<Character> list = new MyLL<>();
+        list.addToBack('x');
+        list.addToBack('m');
+        list.addToBack('v');
+        list.addToBack('q');
+        list.printFromBack();
+
+       // list.remove('m');
+
+        System.out.println("_______TEST________");
+        list.printFromBack();*/
+        MyLL<Integer> intList = new MyLL<>();
+
+        intList.addToBack(5);
+        intList.addToBack(10);
+        intList.addToBack(25);
+        intList.addToBack(400);
+
+        intList.printFromBack();
+
     }
 
     public static void printList(Node start) {
